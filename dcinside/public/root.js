2177a9e0b8e1dd5a98018d5c79c49e571223ea5c
Doo Lee <dlengjs12@naver.com>
2021-12-13 23:44:18 +0900

    Component 작업중

---
+++ dcinside/public/root.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Chat from "./components/Chat";
+ReactDOM.render( /*#__PURE__*/React.createElement(Chat, null), document.getElementById("chat"));
